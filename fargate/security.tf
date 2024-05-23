0cdb6de0feb69076dbee2a4de4ce53f4c13357ef
Remigiusz Pisarski <RPisarski@private.convista.de>
2024-05-23 13:20:10 +0200

    Add real rds

--- fargate/security.tf
+++ fargate/security.tf
@@ -39,3 +39,24 @@ resource "aws_security_group" "ecs_tasks" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+# ALB security Group: Edit to restrict access to the application
+resource "aws_security_group" "rds" {
+    name        = "rds-security-group"
+    description = "controls access to the RDS"
+    vpc_id      = aws_vpc.main.id
+
+    ingress {
+        protocol    = "tcp"
+        from_port   = 5432
+        to_port     = 5432
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        protocol    = "-1"
+        from_port   = 0
+        to_port     = 0
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
